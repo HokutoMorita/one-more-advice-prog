ad16c7ae2a128681d08e99bf8e0244951addfd0d
HokutoMorita <han2210mh@gmail.com>
2021-10-07 20:40:56 +0900

    [add]インフラ層とインターフェース層の追加

---
+++ 02_go_ex/clean_architecture_prog/01_user_api_clean_architecture/go.mod
@@ -0,0 +1,3 @@
+module clean_architecture__user_api
+
+go 1.17
--- 02_go_ex/clean_architecture_prog/01_user_api_clean_architecture/src/app/infrastructure/sqlhandler.go
+++ 02_go_ex/clean_architecture_prog/01_user_api_clean_architecture/src/app/infrastructure/sqlhandler.go
@@ -1,8 +1,10 @@
 package infrastructure
 
 import (
+	"fmt"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"app/interfaces/database"
 )
 
 type SqlHandler struct {
@@ -17,4 +19,42 @@ func NewSqlHandler() *SqlHandler {
 	sqlHandler := new(SqlHandler)
 	sqlHandler.Conn = conn
 	return sqlHandler
+}
+
+func (handler *SqlHandler) Execute(statement string, args ...interface{}) (database.Result, error) {
+	res := SqlResult{}
+	result, err := handler.Conn.Exec(statement, args...)
+	if err != nil {
+		fmt.Println(err)
+		return res, err
+	}
+	res.Result = result
+	return res, nil
+}
+
+func (handler *SqlHandler) Query(statement string, args ...interface{}) (database.Row, error) {
+	rows, err := handler.Conn.Query(statement, args...)
+	if err != nil {
+		fmt.Println(err)
+		return new(SqlRow), err
+	}
+	row := new(SqlRow)
+	row.Rows = rows
+	return row, nil
+}
+
+type SqlRow struct {
+	Rows *sql.Rows
+}
+
+func (r SqlRow) Scan(dest ...interface{}) error {
+	return r.Rows.Scan(dest...)
+}
+
+func (r SqlRow) Next() bool {
+	return r.Rows.Next()
+}
+
+func (r SqlRow) Close() error {
+	return r.Rows.CLose()
 }
